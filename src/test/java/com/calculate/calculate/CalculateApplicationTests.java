52ac972ef7bb8a7c08631f775ea997dc5831875b
jeawook <pjw4825@naver.com>
2021-03-01 17:19:56 +0900

    init project

---
+++ src/test/java/com/calculate/calculate/CalculateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculate.calculate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
