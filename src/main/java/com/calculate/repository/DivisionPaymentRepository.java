7a8193e3aed17f39bb8fedaf4d3a40074244dd9c
jeawook <pjw4825@naver.com>
2021-03-02 21:57:11 +0900

    add repository

---
+++ src/main/java/com/calculate/repository/DivisionPaymentRepository.java
@@ -0,0 +1,7 @@
+package com.calculate.repository;
+
+import com.calculate.domain.DivisionPayment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DivisionPaymentRepository extends JpaRepository<DivisionPayment, Long> {
+}
---
+++ src/main/java/com/calculate/repository/PaymentRepository.java
@@ -0,0 +1,10 @@
+package com.calculate.repository;
+
+import com.calculate.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByToken(String token);
+}
