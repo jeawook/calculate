eda761c6e4b0c49a83f04b061dee72f2a6ea6672
jeawook <pjw4825@naver.com>
2021-03-08 18:33:47 +0900

    Create ResponseMessage.java

---
+++ src/main/java/com/calculate/common/ResponseMessage.java
@@ -0,0 +1,12 @@
+package com.calculate.common;
+
+public final class ResponseMessage {
+
+    private ResponseMessage() {
+
+    }
+
+    public static final String NOT_MATCHED_USER = "권한이 없는 사용자 입니다.";
+    public static final String NOT_MATCHED_ROOM = "권한이 없는 정보입니다.";
+
+}
